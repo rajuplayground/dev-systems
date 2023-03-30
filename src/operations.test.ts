214634062eca8e180ffb308dd597926225a83bde
rajuplayground <rajudevbox@gmail.com>
2023-03-30 12:25:24 +0530

    modified operations

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -1,7 +1,14 @@
-import { expect, it } from "vitest"
-import { add } from "./operations"
+import { expect, it, describe } from "vitest"
+import { add, subtract } from "./operations"
 
-it("should work", ()=>{
-    const result = add(1,2)
-    expect(result).toBe(3)
+describe("Mathematical operations", ()=>{
+    it("should work return added numbers", ()=>{
+        const result = add(1,2)
+        expect(result).toBe(3)
+    })
+
+    it("should work return subtracted numbers", ()=>{
+        const result = subtract(4,2)
+        expect(result).toBe(2)
+    })
 })
