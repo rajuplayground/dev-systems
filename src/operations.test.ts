85e6ebd495f89029f8028ff0952bcb71b2ac87aa
rajuplayground <rajudevbox@gmail.com>
2023-03-30 12:50:42 +0530

    added division

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -1,5 +1,5 @@
 import { expect, it, describe } from "vitest"
-import { add, multiplication, subtract } from "./operations"
+import { add, division, multiplication, subtract } from "./operations"
 
 describe("Mathematical operations", ()=>{
     it("should work return added numbers", ()=>{
@@ -15,5 +15,15 @@ describe("Mathematical operations", ()=>{
     it("should work return multiplied numbers", ()=>{
         const result = multiplication(4,2)
         expect(result).toBe(8)
+    })
+
+    it("should work return 0 if denominator is 0 for division", ()=>{
+        const result = division(4,0)
+        expect(result).toBe(0)
+    })
+
+    it("should work return division", ()=>{
+        const result = division(4,2)
+        expect(result).toBe(2)
     })
 })
--- src/operations.ts
+++ src/operations.ts
@@ -9,3 +9,9 @@ export const subtract = (a,b)=>{
 export const multiplication = (a,b)=>{
     return a * b;
 }
+
+export const division = (a,b)=>{
+    if(b===0)
+        return 0
+    return a/b;
+}
