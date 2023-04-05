cd0412096e823a81c1c13443bbdadb38d9efb46c
rajuplayground <rajudevbox@gmail.com>
2023-04-05 09:28:25 +0530

    added coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,30 @@
-import { mergeConfig } from 'vite'
-import { defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
-import path from 'node:path'
+import { mergeConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import viteConfig from "./vite.config";
+import path from "node:path";
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    globals: true,
-    setupFiles: path.resolve(__dirname, './src/test/setup.ts'),
-    environmentMatchGlobs: [
-        ['**/*.test.tsx', 'jsdom']
-      ]
-  },
-}))
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      setupFiles: path.resolve(__dirname, "./src/test/setup.ts"),
+      environmentMatchGlobs: [["**/*.test.tsx", "jsdom"]],
+      coverage: {
+        statements: 80.59,
+        thresholdAutoUpdate: true,
+        include: ["src/**/*"],
+        exclude: [
+          "test/**",
+          "vite.*.ts",
+          "**/*.d.ts",
+          "**/*.test.*",
+          "**/*.config.*",
+          "**/snapshot-tests/**",
+          "**/coverage/**",
+        ],
+        all: true,
+      },
+    },
+  })
+);
